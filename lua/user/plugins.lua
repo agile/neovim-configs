3ad2da5029e3399132f7229ec0e26266306b5a0d
Mike Vincent <mike@vincent.ws>
2022-08-25 23:34:59 -0500

    Add editconfig plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,11 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"  -- Useful lua functions used by lots of plugins
 
+  -- support for .editorconfig files https://editorconfig.org/
+  use "editorconfig/editorconfig-vim"
+
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
