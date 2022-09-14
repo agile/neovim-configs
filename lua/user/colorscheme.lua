d10da59c08bfa80c14368ce9bfd7fe24faa2078a
Mike Vincent <mike@vincent.ws>
2022-09-13 19:04:34 -0500

    Colorscheme: tokyonight:night

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,13 +1,14 @@
-local colorscheme = "tokyonight"
--- local colorscheme = "catppuccin"
-
--- other settings documented here:
 -- https://github.com/folke/tokyonight.nvim#%EF%B8%8F-configuration
-vim.g.tokyonight_style = "night"  -- storm, night, or day
-vim.g.boo_colorscheme_theme = "forest_stream" -- sunset_cloud, radioactive_waste, forest_stream, crimson_moonlight
+local colorscheme = "tokyonight"
+local theme_config = {style = "night"}
+
 -- https://github.com/catppuccin/nvim
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-require("catppuccin").setup()
+-- local colorscheme = "catppuccin"
+vim.g.catppuccin_flavour = "mocha" -- latte (light theme), frappe, macchiato, mocha
+
+-- Generally themes may have some configuration that can be tweaked and usually
+-- need those tweaks made before the colorscheme is set below
+require(colorscheme).setup(theme_config)
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
