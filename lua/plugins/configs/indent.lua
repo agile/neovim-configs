06ffbf3950cd9fd3e574fc76244949630f2c4184
Mike Vincent <mike@vincent.ws>
2022-10-23 23:23:36 -0500

    Deconfigure indentation markers

--- lua/plugins/configs/indent.lua
+++ lua/plugins/configs/indent.lua
@@ -7,12 +7,16 @@ end
 indent.setup {
     show_end_of_line = true,
     -- space_char_blankline = " ",
-    show_current_context = true,
+    -- show_current_context = true,
     -- show_current_context_start = true,
     -- char_highlight_list = {
     --   "IndentBlanklineIndent1",
     --   "IndentBlanklineIndent2",
     --   "IndentBlanklineIndent3",
+    --   "IndentBlanklineIndent4",
+    --   "IndentBlanklineIndent5",
+    --   "IndentBlanklineIndent6",
+    -- },
 }
 
 
