24ce611b4a1ec96fb5166055a430c905bd820bc4
Mike Vincent <mike@vincent.ws>
2022-12-10 11:35:35 -0600

    Remove old configs

--- lua/user/lsp/init.lua
+++
@@ -1,7 +0,0 @@
-require("user.lsp.configs")           -- where LSP servers are configured, sometimes installed
-require("user.lsp.handlers").setup()  -- generalized on_attach/capabilities/keymaps
-require "user.lsp.null-ls"            -- formatters/linters
-
--- TODO
--- https://www.reddit.com/r/neovim/comments/w5h9tl/lsp_linesnvim_v2_is_out/
--- ^ show LSP diagnostics via virtual lines (github mirror: https://github.com/Maan2003/lsp_lines.nvim)
--- lua/user/mini-trailspace.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, trailspace = pcall(require, "mini.trailspace")
-if not status_ok then
-  return
-end
-
-trailspace.setup()
