58ad6d7196ca86d998a0c717ac095df915387325
Mike Vincent <mike@vincent.ws>
2023-01-02 10:30:15 -0600

    Nicer formatting handling

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,13 @@ local _lsp, lsp = pcall(require, "lsp-zero")
 if not _lsp then
     return
 end
-local util = require "lspconfig.util"
+local util = require("lspconfig.util")
+local _lspformat, lspformat = pcall(require, "lsp-format")
+if _lspformat then
+    lspformat.setup({
+        sync = true,
+    })
+end
 
 -- Turn down the noise
 vim.lsp.set_log_level("WARN")
@@ -10,238 +16,221 @@ vim.lsp.set_log_level("WARN")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  "bashls",
-  "clangd",
-  "dockerls",
-  "elixirls",
-  "gopls",
-  "html",
-  "jsonls",
-  "pyright",
-  "ruby_ls",
-  "rust_analyzer",
-  -- "sqlls",
-  -- "sqls",
-  "sumneko_lua",
-  "terraformls",
-  "tflint",
-  "yamlls",
+    "bashls",
+    "clangd",
+    "dockerls",
+    "elixirls",
+    "gopls",
+    "html",
+    "jsonls",
+    "pyright",
+    "ruby_ls",
+    "rust_analyzer",
+    -- "sqlls",
+    -- "sqls",
+    "sumneko_lua",
+    "terraformls",
+    "tflint",
+    "yamlls",
 })
 
 lsp.nvim_workspace({
-  library = vim.api.nvim_get_runtime_file('', true)
+    library = vim.api.nvim_get_runtime_file("", true),
 })
 
+-- Attempting to get terragrunt loved..
 lsp.configure("terraform-ls", {
     root_dir = util.root_pattern(".terraform", ".terraform-version", ".git"),
-    file_types = {"terraform", "terragrunt", "hcl"},
+    file_types = { "terraform", "terragrunt", "hcl" },
 })
 
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local fmt_group = vim.api.nvim_create_augroup("LspFormatting", {})
-
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  if client.server_capabilities.documentSymbolProvider then
-    require('nvim-navic').attach(client, bufnr)
-  end
-
-  if client.supports_method("textDocument/formatting") then
-    if client.name ~= "null-ls" then
-      print("LSP client " .. client.name .. " supports formatting")
+    if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, bufnr)
     end
-    vim.api.nvim_clear_autocmds({ group = fmt_group, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        group = fmt_group,
-        buffer = bufnr,
-        callback = function()
-            lsp_formatting(bufnr)
-        end,
-    })
-  end
 
-  if client.name == "eslint" then
-    vim.cmd.LspStop("eslint")
-    return
-  end
+    require("lsp-format").on_attach(client)
 
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
+
+------------------------------
+-- Completion Setup
+------------------------------
 
 -- see https://github.com/lttr/cmp-jira
 local _cmp_jira, cmp_jira = pcall(require, "cmp_jira")
 if _cmp_jira then
-  cmp_jira.setup()
+    cmp_jira.setup()
 end
 
 -- Additional completion sources
 local cmp_sources = lsp.defaults.cmp_sources()
-table.insert(cmp_sources, {name = "cmp_jira"}) -- complete JIRA references
-table.insert(cmp_sources, {name = "plugins"})  -- complete gitup refs for plugins
-table.insert(cmp_sources, {name = "nvim_lua"}) -- neovim lua api
-table.insert(cmp_sources, {name = "nvim_lsp_signature_help"}) -- display function signatures
+table.insert(cmp_sources, { name = "cmp_jira" }) -- complete JIRA references
+table.insert(cmp_sources, { name = "plugins" }) -- complete gitup refs for plugins
+table.insert(cmp_sources, { name = "nvim_lua" }) -- neovim lua api
+table.insert(cmp_sources, { name = "nvim_lsp_signature_help" }) -- display function signatures
 
 local _lspkind, lspkind = pcall(require, "lspkind")
 local _devicons, devicons = pcall(require, "nvim-web-devicons")
 
 local cmp_formatting = {}
 if _lspkind and _devicons then
-  -- TODO, maybe: https://github.com/onsails/lspkind.nvim/pull/30
-  cmp_formatting = {
-    fields = {"kind", "abbr", "menu"},
-    format = function(entry, item)
-      if vim.tbl_contains({"path"}, entry.source.name) then
-        local icon, hl_group = devicons.get_icon(entry:get_completion_item().label)
-        if icon then
-          item.kind = icon
-          item.kind_hl_group = hl_group
-          return item
-        end
-      end
+    -- TODO, maybe: https://github.com/onsails/lspkind.nvim/pull/30
+    cmp_formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, item)
+            if vim.tbl_contains({ "path" }, entry.source.name) then
+                local icon, hl_group = devicons.get_icon(entry:get_completion_item().label)
+                if icon then
+                    item.kind = icon
+                    item.kind_hl_group = hl_group
+                    return item
+                end
+            end
 
-      local menu = {
-          buffer   = "[BUF]",
-          cmp_jira = "[JIRA]",
-          luasnip  = "[SNIPPET]",
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[NVIM]",
-          path     = "[PATH]",
-          plugins  = "[PLUGIN]",
-      }
-      local kind_name = item.kind
+            local menu = {
+                buffer = "[BUF]",
+                cmp_jira = "[JIRA]",
+                luasnip = "[SNIPPET]",
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[NVIM]",
+                path = "[PATH]",
+                plugins = "[PLUGIN]",
+            }
+            local kind_name = item.kind
 
-      item = lspkind.cmp_format({
-        -- use vscode icons
-        -- preset = "codicons",
-        preset = "default",
-        mode = "symbol_text",   -- symbol, symbol_text, text, text_symbol
-        maxwidth = 100,         -- truncate long entries
-        ellipsis_char = "...",  -- inidcator to append to truncated entries
-        menu = (menu),
-      })(entry, item)
+            item = lspkind.cmp_format({
+                -- use vscode icons
+                -- preset = "codicons",
+                preset = "default",
+                mode = "symbol_text", -- symbol, symbol_text, text, text_symbol
+                maxwidth = 100, -- truncate long entries
+                ellipsis_char = "...", -- inidcator to append to truncated entries
+                menu = menu,
+            })(entry, item)
 
-      -- could just yield item as lspkind formatted it but reformatting a little more to place the
-      -- icon on the left of the suggestion..
-      local strings = vim.split(item.kind, "%s", { trimempty = true })
-      item.kind = " " .. strings[1] .. " "
-      item.menu = " " .. (menu[entry.source.name] or "[???]") .." (" .. kind_name .. ") "
+            -- could just yield item as lspkind formatted it but reformatting a little more to place the
+            -- icon on the left of the suggestion..
+            local strings = vim.split(item.kind, "%s", { trimempty = true })
+            item.kind = " " .. strings[1] .. " "
+            item.menu = " " .. (menu[entry.source.name] or "[???]") .. " (" .. kind_name .. ") "
 
-      return item
-    end,
-  }
+            return item
+        end,
+    }
 end
 
 lsp.setup_nvim_cmp({
-  -- configure completetion to not auto-select suggestions
-  preselect = "none",  -- do not auto-select first item in list
-  completion = {
-    completeopt = "menu,menuone,noinsert,noselect",
-  },
-  select_behavior = "select", -- insert: implicitly insert selection, select: NO IMPLICIT INSERTION
-
-  sources = cmp_sources,
-  window = {
+    -- configure completetion to not auto-select suggestions
+    preselect = "none", -- do not auto-select first item in list
     completion = {
-      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-      col_offset = -3,
-      side_padding = 0,
+        completeopt = "menu,menuone,noinsert,noselect",
     },
+    select_behavior = "select", -- insert: implicitly insert selection, select: NO IMPLICIT INSERTION
 
-  },
-  formatting = cmp_formatting,
+    sources = cmp_sources,
+    window = {
+        completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+        },
+    },
+    formatting = cmp_formatting,
 })
 
 lsp.setup()
 
+------------------------------
+-- POST LSP Zero Setup
+------------------------------
+
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = true,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = false,
+    float = true,
 })
 
 -- Initialize Mason/null-ls, needs to be done after lsp.setup()
 -- https://github.com/VonHeikemen/lsp-zero.nvim/issues/60#issuecomment-1363800412
 local _mason_nls, mason_nls = pcall(require, "mason-null-ls")
 if _mason_nls then
-  mason_nls.setup({
-    automatic_installation = true,
-    automatic_setup = true,
-    ensure_installed = {
-      "jq",
-      "hadolint",
-      "black",
-      "isort",
-      "pylint",
-      "rubocop",
-      "shellcheck",
-      "stylelua",
-      --"sql_formatter",
-    },
-  })
-end
+    mason_nls.setup({
+        automatic_installation = true,
 
+        -- if null-ls is the source of truth
+        automatic_setup = false,
+        ensure_installed = nil,
+
+        -- if mason-null-ls is the source of truth
+        -- automatic_setup = true,
+        -- ensure_installed = {
+        --     "jq",
+        --     "hadolint",
+        --     "black",
+        --     "isort",
+        --     "pylint",
+        --     "rubocop",
+        --     "shellcheck",
+        --     "stylelua",
+        --     --"sql_formatter",
+        -- },
+    })
+end
 
 local _null_ls, null_ls = pcall(require, "null-ls")
 if _null_ls then
-  local sources = {
-    -- docker
-    null_ls.builtins.diagnostics.hadolint,
-    -- json
-    null_ls.builtins.formatting.jq,
-    -- lua
-    null_ls.builtins.formatting.stylua,
-    -- packer
-    null_ls.builtins.formatting.packer,
-    -- python
-    null_ls.builtins.formatting.black.with({
-      extra_args = { "--line-length=120" }
-    }),
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.pylint,
-    -- R
-    null_ls.builtins.formatting.format_r,
-    -- ruby
-    null_ls.builtins.diagnostics.rubocop,
-    null_ls.builtins.formatting.rubocop,
-    -- rust
-    null_ls.builtins.formatting.rustfmt,
-    -- scala
-    null_ls.builtins.formatting.scalafmt,
-    -- shell
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.hover.printenv,
-    -- SQL
-    null_ls.builtins.formatting.pg_format,
-    -- terraform
-    null_ls.builtins.formatting.terraform_fmt,
+    local sources = {
+        -- docker
+        null_ls.builtins.diagnostics.hadolint,
+        -- json
+        null_ls.builtins.formatting.jq,
+        -- lua
+        null_ls.builtins.formatting.stylua,
+        -- packer
+        null_ls.builtins.formatting.packer,
+        -- python
+        null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length=120" },
+        }),
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.diagnostics.pylint,
+        -- R
+        null_ls.builtins.formatting.format_r,
+        -- ruby
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+        -- rust
+        null_ls.builtins.formatting.rustfmt,
+        -- scala
+        null_ls.builtins.formatting.scalafmt,
+        -- shell
+        null_ls.builtins.code_actions.shellcheck,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.hover.printenv,
+        -- SQL
+        null_ls.builtins.formatting.pg_format,
+        -- terraform
+        null_ls.builtins.formatting.terraform_fmt,
 
-    -- etc..
-    null_ls.builtins.diagnostics.trail_space,
-  }
+        -- etc..
+        null_ls.builtins.diagnostics.trail_space,
+    }
 
-  null_ls.setup({ sources = sources })
+    null_ls.setup({ sources = sources })
 end
--- lua/mysetup/packer.lua
+++ lua/mysetup/packer.lua
@@ -1,10 +1,10 @@
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
-  install_path }
+    PACKER_BOOTSTRAP =
+        vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 -- kind of prefer to be explicit about when I want to run PackerSync?
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -17,139 +17,141 @@ vim.cmd [[packadd packer.nvim]]
 
 local ok, packer = pcall(require, "packer")
 if not ok then
-  return
+    return
 end
 
 packer.startup(function(use)
-  ---- Package manager
-  use "wbthomason/packer.nvim"
+    ---- Package manager
+    use("wbthomason/packer.nvim")
 
-  use {
-    -- Search engine
-    "nvim-telescope/telescope.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-  }
+    use({
+        -- Search engine
+        "nvim-telescope/telescope.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
 
-  -- themes
-  use "~/personal/lushy-blues"
-  use "rktjmp/lush.nvim"
-  use "Mofiqul/vscode.nvim"
-  use "folke/tokyonight.nvim"
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
-  use {
-    "catppuccin/nvim",
-    as = "catppuccin",
-  }
-  use {
-    "rose-pine/neovim",
-    as = "rose-pine",
-  }
+    -- themes
+    use("~/personal/lushy-blues")
+    use("rktjmp/lush.nvim")
+    use("Mofiqul/vscode.nvim")
+    use("folke/tokyonight.nvim")
+    use("navarasu/onedark.nvim") -- Theme inspired by Atom
+    use({
+        "catppuccin/nvim",
+        as = "catppuccin",
+    })
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
+    })
 
-  use "NvChad/nvim-colorizer.lua"
+    use("NvChad/nvim-colorizer.lua")
 
-  ---- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    requires = {
-      "p00f/nvim-ts-rainbow",                         -- Open/Close node coloring
-      "nvim-treesitter/nvim-treesitter-textobjects",  -- identify comments
-      "nvim-treesitter/playground",                   -- View treesitter information directly in Neovim
-      "JoosepAlviste/nvim-ts-context-commentstring",  -- commentstring
-      "phelipetls/jsonpath.nvim",                     -- Show jq like path in json under cursor
-      -- "the-mikedavis/tree-sitter-diff",            -- grammar for diff output
-    },
-    run = ":TSUpdate",
-  }
+    ---- Treesitter
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        requires = {
+            "p00f/nvim-ts-rainbow", -- Open/Close node coloring
+            "nvim-treesitter/nvim-treesitter-textobjects", -- identify comments
+            "nvim-treesitter/playground", -- View treesitter information directly in Neovim
+            "JoosepAlviste/nvim-ts-context-commentstring", -- commentstring
+            "phelipetls/jsonpath.nvim", -- Show jq like path in json under cursor
+            -- "the-mikedavis/tree-sitter-diff",            -- grammar for diff output
+        },
+        run = ":TSUpdate",
+    })
 
-  -- Conveniences
-  use { "theprimeagen/harpoon" }
+    -- Conveniences
+    use({ "theprimeagen/harpoon" })
 
-  -- LSP
-  use {
-    "VonHeikemen/lsp-zero.nvim",
-    requires = {
-      -- LSP Support
-      {"neovim/nvim-lspconfig"},
-      {"jose-elias-alvarez/null-ls.nvim"},
-      {"williamboman/mason.nvim"},
-      {"williamboman/mason-lspconfig.nvim"},
-      {"jay-babu/mason-null-ls.nvim"},
+    -- LSP
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "jose-elias-alvarez/null-ls.nvim" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "jay-babu/mason-null-ls.nvim" },
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
+            -- Formatting config helper
+            { "lukas-reineke/lsp-format.nvim" },
 
-      { -- nvim-cmp extensions
-        "onsails/lspkind.nvim",                 -- VSCode like item type icons
-        "kyazdani42/nvim-web-devicons",         -- MOAR ICONS
-        "hrsh7th/cmp-buffer",                   -- Buffer completions
-        "hrsh7th/cmp-calc",                     -- Math completions
-        "hrsh7th/cmp-cmdline",                  -- Command line completions
-        "hrsh7th/cmp-nvim-lsp",                 -- LSP completions
-        "hrsh7th/cmp-nvim-lsp-document-symbol", -- LSP textDocument/documentSymbol completions
-        "hrsh7th/cmp-nvim-lsp-signature-help",  -- LSP Signature completions
-        "hrsh7th/cmp-nvim-lua",                 -- Lua completions
-        "hrsh7th/cmp-path",                     -- Path completions
-        "lukas-reineke/cmp-under-comparator",   -- Better sort completion items starting with underscore (Python)
-        "lttr/cmp-jira",                        -- JIRA completions
-        "KadoBOT/cmp-plugins",                  -- Neovim plugin completions
-        "saadparwaiz1/cmp_luasnip",             -- Snippet completions
-      },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
-    },
-  }
+            { -- nvim-cmp extensions
+                "onsails/lspkind.nvim", -- VSCode like item type icons
+                "kyazdani42/nvim-web-devicons", -- MOAR ICONS
+                "hrsh7th/cmp-buffer", -- Buffer completions
+                "hrsh7th/cmp-calc", -- Math completions
+                "hrsh7th/cmp-cmdline", -- Command line completions
+                "hrsh7th/cmp-nvim-lsp", -- LSP completions
+                "hrsh7th/cmp-nvim-lsp-document-symbol", -- LSP textDocument/documentSymbol completions
+                "hrsh7th/cmp-nvim-lsp-signature-help", -- LSP Signature completions
+                "hrsh7th/cmp-nvim-lua", -- Lua completions
+                "hrsh7th/cmp-path", -- Path completions
+                "lukas-reineke/cmp-under-comparator", -- Better sort completion items starting with underscore (Python)
+                "lttr/cmp-jira", -- JIRA completions
+                "KadoBOT/cmp-plugins", -- Neovim plugin completions
+                "saadparwaiz1/cmp_luasnip", -- Snippet completions
+            },
 
-  -- Status line
-  use {
-    "nvim-lualine/lualine.nvim",
-    requires = {
-      "arkav/lualine-lsp-progress",
-      { -- statusline/winbar component to display code context
-        "SmiteshP/nvim-navic",
-      },
-      -- "rmagatti/auto-session",
-      { "kyazdani42/nvim-web-devicons" },
-    },
-  }
-  -- use {
-  --   -- buffer/tab decorations/styles
-  --   "akinsho/bufferline.nvim",
-  --   requires = {
-  --     "kyazdani42/nvim-web-devicons",
-  --     -- delete buffers/close files
-  --     -- maybe check out one of the derivatives https://github.com/moll/vim-bbye/network
-  --     -- or https://github.com/famiu/bufdelete.nvim
-  --     -- or https://github.com/kazhala/close-buffers.nvim
-  --     -- or https://github.com/marklcrns/vim-smartq
-  --     "moll/vim-bbye",
-  --   },
-  -- }
-  use {
-    -- Add git related info in the signs columns and popups
-    "lewis6991/gitsigns.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-  }
-  -- Rust LSP extensions
-  use {
-    "simrat39/rust-tools.nvim",
-    requires = "nvim-lua/plenary.nvim"
-  }
-  -- Scala
-  use {
-    "scalameta/nvim-metals",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "mfussenegger/nvim-dap",
-    },
-  }
-  -- Lookml Syntax
-  use "thalesmello/lkml.vim"
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        },
+    })
 
-  -- Utilities
-  use "gpanders/editorconfig.nvim" -- support for .editorconfig files https://editorconfig.org/
-  use { "godlygeek/tabular", cmd = { "Tabularize" } } -- align the things!
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+    -- Status line
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = {
+            "arkav/lualine-lsp-progress",
+            { -- statusline/winbar component to display code context
+                "SmiteshP/nvim-navic",
+            },
+            -- "rmagatti/auto-session",
+            { "kyazdani42/nvim-web-devicons" },
+        },
+    })
+    -- use {
+    --   -- buffer/tab decorations/styles
+    --   "akinsho/bufferline.nvim",
+    --   requires = {
+    --     "kyazdani42/nvim-web-devicons",
+    --     -- delete buffers/close files
+    --     -- maybe check out one of the derivatives https://github.com/moll/vim-bbye/network
+    --     -- or https://github.com/famiu/bufdelete.nvim
+    --     -- or https://github.com/kazhala/close-buffers.nvim
+    --     -- or https://github.com/marklcrns/vim-smartq
+    --     "moll/vim-bbye",
+    --   },
+    -- }
+    use({
+        -- Add git related info in the signs columns and popups
+        "lewis6991/gitsigns.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    -- Rust LSP extensions
+    use({
+        "simrat39/rust-tools.nvim",
+        requires = "nvim-lua/plenary.nvim",
+    })
+    -- Scala
+    use({
+        "scalameta/nvim-metals",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "mfussenegger/nvim-dap",
+        },
+    })
+    -- Lookml Syntax
+    use("thalesmello/lkml.vim")
 
+    -- Utilities
+    use("gpanders/editorconfig.nvim") -- support for .editorconfig files https://editorconfig.org/
+    use({ "godlygeek/tabular", cmd = { "Tabularize" } }) -- align the things!
+    use("tpope/vim-sleuth") -- Detect tabstop and shiftwidth automatically
 end)
