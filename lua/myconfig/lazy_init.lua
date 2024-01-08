c28107d1fa551693f69de770f431a045a2edd1f9
Mike Vincent <mike@vincent.ws>
2024-01-07 23:24:03 -0600

    Check lazy out to the sha pinned in the lockfile

--- lua/myconfig/lazy_init.lua
+++ lua/myconfig/lazy_init.lua
@@ -1,13 +1,35 @@
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.notify("Installing lazy...")
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+
+
+-- Update lazy to use the exact sha we've got pinned
+-- https://github.com/folke/lazy.nvim/issues/287#issuecomment-1370876298
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
+  vim.notify("Installing package manager...")
   vim.fn.system({
     "git",
     "clone",
-    "--filter=blob:none",
+    "--single-branch",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
     lazypath,
   })
+  local f = io.open(vim.fn.stdpath("config") .. "/lazy-lock.json", "r")
+  if f then
+    local data = f:read("*a")
+    local lock = vim.json.decode(data)
+    vim.fn.system { "git", "-C", lazypath, "checkout", lock["lazy.nvim"].commit }
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
