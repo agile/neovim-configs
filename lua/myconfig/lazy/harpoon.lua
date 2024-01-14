79434e13ea275a71189efb3be9a77e32d68bcdd6
Mike Vincent <mike@vincent.ws>
2024-01-13 22:33:46 -0600

    Redo harpoon mappings

--- lua/myconfig/lazy/harpoon.lua
+++ lua/myconfig/lazy/harpoon.lua
@@ -1,16 +1,16 @@
 return {
-      "theprimeagen/harpoon",
-      config = function()
-        local mark = require("harpoon.mark")
+  "theprimeagen/harpoon",
+  config = function()
+    local mark = require("harpoon.mark")
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
+    vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon file" })
 
-        local ui = require("harpoon.ui")
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    local ui = require("harpoon.ui")
+    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon menu" })
 
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-      end
+    vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end, { desc = "Harpoon file 1" })
+    vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end, { desc = "Harpoon file 2" })
+    vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end, { desc = "Harpoon file 3" })
+    vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end, { desc = "Harpoon file 4" })
+  end
 }
