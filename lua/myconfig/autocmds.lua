dd418692d8288c2ce39e37c5fe1c8c0ed0ee4993
Mike Vincent <mike@vincent.ws>
2024-05-05 08:17:14 -0500

    formatted

--- lua/myconfig/autocmds.lua
+++ lua/myconfig/autocmds.lua
@@ -3,32 +3,32 @@ local autogrp = vim.api.nvim_create_augroup("MyAutoCommands", { clear = true })
 
 -- Highlight on yank
 autocmd("TextYankPost", {
-  group = autogrp,
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
-      timeout = 40,
-    })
-  end,
+    group = autogrp,
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 40,
+        })
+    end,
 })
 
 -- Disable comment new line
 autocmd("BufWinEnter", {
-  group = autogrp,
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove { "c", "r", "o" }
-  end,
+    group = autogrp,
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
 })
 
 -- Override filetypes
-autocmd({"BufRead","BufNewFile"}, {
-  group = autogrp,
-  pattern = "tsconfig.json",
-  callback = function()
-    vim.opt.filetype = "jsonc"
-  end,
+autocmd({ "BufRead", "BufNewFile" }, {
+    group = autogrp,
+    pattern = "tsconfig.json",
+    callback = function()
+        vim.opt.filetype = "jsonc"
+    end,
 })
 -- autocmd("BufRead,BufNewFile", {
 --     group = autogrp,
@@ -72,10 +72,10 @@ autocmd({"BufRead","BufNewFile"}, {
 
 -- autocmd("BufReadPost COMMIT_EDITMSG"
 autocmd("BufReadPost", {
-  group = autogrp,
-  pattern = "*COMMIT_EDITMSG",
-  callback = function()
-    vim.cmd([[
+    group = autogrp,
+    pattern = "*COMMIT_EDITMSG",
+    callback = function()
+        vim.cmd([[
       exe "vnew" |
       exe "set bt=nofile ft=diff" |
       exe "silent r! git diff --cached" |
@@ -83,7 +83,7 @@ autocmd("BufReadPost", {
       exe "normal :wincmd w" |
       exe "normal ggi"
     ]])
-  end,
+    end,
 })
 
 -- "jump to last cursor position when opening a file
@@ -98,15 +98,15 @@ autocmd("BufReadPost", {
 -- autocmd BufReadPost * call SetCursorPosition()
 --
 -- This should effectively be the same..
-autocmd('BufReadPost', {
-  callback = function()
-    -- if it's not a commit message, jump to last position we were at in the file
-    if not vim.bo.filetype:match("^.*commit$") then
-      local mark = vim.api.nvim_buf_get_mark(0, '"')
-      local lcount = vim.api.nvim_buf_line_count(0)
-      if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-      end
-    end
-  end,
+autocmd("BufReadPost", {
+    callback = function()
+        -- if it's not a commit message, jump to last position we were at in the file
+        if not vim.bo.filetype:match("^.*commit$") then
+            local mark = vim.api.nvim_buf_get_mark(0, '"')
+            local lcount = vim.api.nvim_buf_line_count(0)
+            if mark[1] > 0 and mark[1] <= lcount then
+                pcall(vim.api.nvim_win_set_cursor, 0, mark)
+            end
+        end
+    end,
 })
