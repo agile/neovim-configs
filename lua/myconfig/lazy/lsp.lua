900e574ff335d73ba97e1b05a5231a539ec7dc6c
Mike Vincent <mike@vincent.ws>
2024-05-05 08:16:33 -0500

    Package updates

--- lua/myconfig/lazy/lsp.lua
+++ lua/myconfig/lazy/lsp.lua
@@ -1,21 +1,21 @@
 local lang_servers = {
-    "bashls",        -- bash
-    "clangd",        -- c/c++
-    "cmake",         -- cmake language server
-    "cssls",         -- css language server
-    "dockerls",      -- dockerfile
-    "elixirls",      -- elixir
-    "gopls",         -- go
-    "html",          -- html language server
-    "jdtls",         -- java
-    "jsonnet_ls",    -- jsonnet language server
-    "lua_ls",        -- lua
-    "pyright",       -- python
-    "ruff_lsp",      -- extremely fast Python linter and code transformation
+    "bashls", -- bash
+    "clangd", -- c/c++
+    "cmake", -- cmake language server
+    "cssls", -- css language server
+    "dockerls", -- dockerfile
+    "elixirls", -- elixir
+    "gopls", -- go
+    "html", -- html language server
+    "jdtls", -- java
+    "jsonnet_ls", -- jsonnet language server
+    "lua_ls", -- lua
+    "pyright", -- python
+    "ruff_lsp", -- extremely fast Python linter and code transformation
     "rust_analyzer", -- rust
     -- "sqls",          -- SQL
-    "terraformls",   -- terraform hcl
-    "tflint",        -- terraform lint
+    "terraformls", -- terraform hcl
+    "tflint", -- terraform lint
 }
 if vim.g.has_nix then
     -- nix language server
@@ -23,6 +23,8 @@ if vim.g.has_nix then
 end
 
 return {
+    -- Neovim dev conveniences
+    { "folke/neodev.nvim" },
 
     -- Rust LSP extensions
     {
@@ -44,15 +46,37 @@ return {
         "jalvesaq/Nvim-R",
     },
 
+    -- UI for lsp messages
+    {
+        "j-hui/fidget.nvim",
+        event = { "LspAttach" },
+        opts = {
+            progress = {
+                display = {
+                    done_ttl = 10, -- how long a message should persist after completion
+                },
+            },
+            notification = {
+                filter = vim.log.levels.DEBUG, -- Minimum notification level, default INFO
+                -- view = { stack_upwards = false, },
+                window = {
+                    normal_hl = "Search", -- Base highlight group in notification window
+                },
+            },
+        },
+    },
+
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "lukas-reineke/lsp-format.nvim",
-            "SmiteshP/nvim-navic",
+            "williamboman/mason.nvim", -- lsp server installer
+            "williamboman/mason-lspconfig.nvim", -- lsp config helper
+            "lukas-reineke/lsp-format.nvim", -- wrapper for lsp formatting
+            "SmiteshP/nvim-navic", -- code org crumbs
+            "nanotee/sqls.nvim", -- bindings/events for SQLs server
         },
         config = function()
+            require("neodev").setup({})
             local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
             local capabilities = require("cmp_nvim_lsp").default_capabilities(lsp_capabilities)
             require("mason").setup()
@@ -64,32 +88,33 @@ return {
                     -- a dedicated handler.
                     function(server_name) -- default handler (optional)
                         require("lspconfig")[server_name].setup({
-                            capabilties = capabilities
+                            capabilties = capabilities,
                         })
                     end,
                     -- Next, you can provide targeted overrides for specific servers.
                     ["rust_analyzer"] = function()
                         require("rust-tools").setup({
-                            capabilties = capabilities
+                            capabilties = capabilities,
                         })
                     end,
                     ["lua_ls"] = function()
                         local lspconfig = require("lspconfig")
-                        lspconfig.lua_ls.setup {
+                        lspconfig.lua_ls.setup({
                             capabilties = capabilities,
                             settings = {
                                 Lua = {
                                     diagnostics = {
-                                        globals = { "vim" }
-                                    }
-                                }
-                            }
-                        }
+                                        globals = { "vim" },
+                                    },
+                                },
+                            },
+                        })
                     end,
                 },
             })
 
             -- Global mappings.
+
             -- See `:help vim.diagnostic.*` for documentation on any of the below functions
             vim.diagnostic.config({
                 virtual_text = true,
@@ -100,15 +125,15 @@ return {
                 float = true,
             })
 
-            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+            vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
             -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
             -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+            vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
             -- Use LspAttach autocommand to only map the following keys
             -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(args)
                     local bufnr = args.buf
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
@@ -121,7 +146,10 @@ return {
                     if client.server_capabilities.documentSymbolProvider then
                         require("nvim-navic").attach(client, bufnr)
                     end
-                    require("lsp-format").on_attach(client)
+
+                    --
+                    -- require("lsp-format").on_attach(client)
+                    require("sqls").on_attach(client, bufnr)
 
                     vim.g.diagnostics_visible = true
                     local function toggle_diagnostics()
@@ -137,24 +165,28 @@ return {
                     -- Buffer local mappings.
                     -- See `:help vim.lsp.*` for documentation on any of the below functions
                     local opts = { buffer = args.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-                    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                    vim.keymap.set('n', '<leader>wl', function()
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wl", function()
                         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                     end, opts)
                     -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set("n", "<leader>D", toggle_diagnostics,
-                        vim.tbl_extend("force", opts, { desc = "lsp toggle diagnostics" }))
-                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    vim.keymap.set('n', '<leader>f', function()
-                        vim.lsp.buf.format { async = true }
+                    vim.keymap.set(
+                        "n",
+                        "<leader>D",
+                        toggle_diagnostics,
+                        vim.tbl_extend("force", opts, { desc = "lsp toggle diagnostics" })
+                    )
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "<leader>f", function()
+                        vim.lsp.buf.format({ async = true })
                     end, opts)
                 end,
             })
