8bbdfd8992440f75219891e17a436fc5534d5f57
Mike Vincent <mike@vincent.ws>
2024-11-03 18:15:56 -0600

    Configure handler for nixd

--- lua/myconfig/lazy/lsp.lua
+++ lua/myconfig/lazy/lsp.lua
@@ -110,6 +110,29 @@ return {
                             },
                         })
                     end,
+                    ["nixd"] = function()
+                        require("lspconfig").nixd.setup({
+                            cmd = { "nixd" },
+                            settings = {
+                                nixd = {
+                                    nixpkgs = {
+                                        expr = "import <nixpkgs> { }",
+                                    },
+                                    formatting = {
+                                        command = { "alejandra" }, -- or nixfmt or nixpkgs-fmt
+                                    },
+                                    -- options = {
+                                    --   nixos = {
+                                    --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
+                                    --   },
+                                    --   home_manager = {
+                                    --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
+                                    --   },
+                                    -- },
+                                },
+                            },
+                        })
+                    end,
                 },
             })
 
